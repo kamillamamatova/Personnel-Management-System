104baa097baa5cc71af4cbce143b79ddaba461f7
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-07-24 23:35:47 -0400

    Update personnelManagementSystem.java

--- personnelManagementSystem.java
+++ personnelManagementSystem.java
@@ -19,6 +19,7 @@ public class FinalProject{
       System.out.println("\n\t\t\tWelcome to the Personnel Management System\nChoose one of the options:");
       System.out.println("1- Enter the information of a faculty");
       System.out.println("2- Enter the information of a student");
+      System.out.println("3- Print tuition invoice for a student");
     }
   }
 }
