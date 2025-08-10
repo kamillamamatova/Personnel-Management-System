62363b3339e72359112fb546ad4e2ed5d073b39d
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-08-10 14:34:23 -0400

    Update personnelManagementSystem.java

--- personnelManagementSystem.java
+++ personnelManagementSystem.java
@@ -98,6 +98,7 @@ public class FinalProject{
 
       // Duplicate ID
       if(findPersonById(id) != null){
+        System.out.println("Duplicate ID. Must be a different ID.\nTry again.");
       }
     }
   }
