ff1b768e1e002ade84a62d51e79e1bac5da8d6af
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-08-12 22:47:52 -0400

    Update personnelManagementSystem.java

--- personnelManagementSystem.java
+++ personnelManagementSystem.java
@@ -131,6 +131,7 @@ public class FinalProject{
 
       // Validate ID format
       if(!id.matches("[a-zA-Z]{2}\\d{4}")){
+        System.out.println("Invalid ID Format. Must be LetterLetterDigitDigitDigitDigit");
       }
     }
   }
