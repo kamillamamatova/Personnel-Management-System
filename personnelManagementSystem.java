ae05f60f735afb632510604b66ef9a76731ba45f
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-08-27 15:34:21 -0400

    Update personnelManagementSystem.java

--- personnelManagementSystem.java
+++ personnelManagementSystem.java
@@ -223,6 +223,7 @@ public class FinalProject{
   static void exitProgram(){
     System.out.print("Would you like to create the report? (Y/N): ");
     if(input.nextLine().equalsIgnoreCase("Y")){
+      System.out.print("Would you like to sort your students by GPA or name? (Enter 1 for GPA. Enter 2 for name): ");
     }
   }
 }
